3a396a8afb1d42d693afd3efa68e46f9a16ebdc1
Hamid <halavatoje@gmail.com>
2024-03-02 23:25:17 +0500

    new pr

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import './App.css';
 
 function App() {
   const TG = window.Telegram.WebApp
-  const { first_name } = TG.initDataUnsafe.user;
+  // const { first_name } = TG.initDataUnsafe.user;
 
   TG.expand()
   return (
     <div className="App">
-      <h1>Hello world, {first_name ? first_name : 'User'}!</h1>
-      <p>Helloo</p>
+
+      <div className="text">
+        <h3>Добро пожаловать!</h3>
+        <h2>USERNAME</h2>
+      </div>
+
+      <p className="hint">
+        Пожалуйста введите ваше имя, адрес электронной почты и пароль.
+      </p>
+
+      <form className="form">
+        <div className="inp">
+          <p className="inpText">Name</p>
+          <input type="text" className='username' />
+        </div>
+        <div className="inp">
+          <p className="inpText">Email</p>
+          <input type="email" className='username' />
+        </div>
+        <div className="inp">
+          <p className="inpText">Password</p>
+          <input type="password" className='password' />
+        </div>
+        <label>
+          <input type="checkbox" />
+          <p className="hint">Соглашаюсь с <a href='https://ya.ru'>условиями</a> и быть хорошим мальчиком.</p>
+        </label>
+
+      </form>
+
     </div>
   );
 }
