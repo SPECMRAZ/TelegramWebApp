34dae4ae2ea80a02251c393a4421b0bdc19a866b
Hamid <halavatoje@gmail.com>
2024-03-02 20:41:03 +0500

    Add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const TG = window.Telegram.WebApp
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world, beaches!
+      git remote add origin https://github.com/SPECMRAZ/TelegramWebApp.git
+git branch -M main
+git push -u origin main
+      </h1>
+      {TG === true ? <p>TG!</p> : <p>No TG!</p>}
     </div>
   );
 }
